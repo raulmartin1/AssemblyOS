c84521f8ab7c29f40b6b8badf723a608ab6bca04
73F4N <stefanoctavian.tabirca@estudiants.urv.cat>
2024-12-13 16:11:47 +0100

    progP: hecha funcion adicional de fase2. NO probada. Falta escribir en panatlla inferior en _gp_rsiTIMER0

--- GARLIC_OS/source/garlic_itcm_proc.s
+++ GARLIC_OS/source/garlic_itcm_proc.s
@@ -313,6 +313,11 @@ _gp_restaurarProc:
 
 
 	.global _gp_waitS
+	@; Rutina para bloquear un proceso.
+	@;Parametros
+	@; R0: semaforo en el que el proceso se bloqueara (0..7)
+	@;Resultado
+	@; R0: 0 si no se ha bloqueado, 1 si se ha bloqueado
 _gp_waitS:
 	push {r1-r2, lr}
 
@@ -330,16 +335,26 @@ _gp_waitS:
 	moveq r0, #0			@; mover un 0 a R0 para devolverlo como resultado
 	beq .LreturnWait		@; acabar rutina indicando que el semaforo ya estaba bloqueado
 
-	@; Bucle para bloquear proceso hasta que el semaforo se libere
+	@; Bloquear proceso en el semaforo
+	bl _gp_inhibirIRQs
 	mov r2, #0				@; en caso de estar a 1 (libre), mover un 0 en R2
-	strb r2, [r1, r0]		@; y guardarlo en el semaforo indicado por parametro
-.LcheckLoop:
-	ldrb r2, [r1, r0]		@; obtener valor del semaforo nuevamente
-	cmp r2, #0				@; comprobar si sigue a 0 (bloqueado)
-	movhi r0, #1			@; en caso de estar a 1 (libre), mover un 1 en R1
-	bhi .LreturnWait		@; y acabar la rutina indicando que el proceso se ha bloqueado correctamente
-	bl _gp_WaitForVBlank	@; si no, esperar retroceso vertical
-	b .LcheckLoop			@; y volver a iterar el bucle
+	strb r2, [r1, r0]		@; y guardarlo en el semaforo indicado por parametro para cerrarlo
+
+	ldr r1, =_gd_pidz		@; cargar direccion de _gd_pidz
+	ldr r2, [r1]			@; obtener su valor
+	orr r2, #0x80000000		@; poner el bit de mas peso a 1 para no ponerlo en la cola RDY
+	str r2, [r1]			@; guardar el valor modificado
+
+	ldr r1, =_gd_qBlock		@; cargar direccion de la lista BLK
+	and r2, #0xF			@; obtener el zocalo actual
+	strb r2, [r1, r0]		@; guardar zocalo en la lista BLK en la posicion indicada por el semaforo (parametro)
+
+	ldr r1, =_gd_nBlock		@; cargar direccion del numero de procesos en la lista BLK
+	ldr r2, [r1]			@; obtener su valor
+	add r2, #1				@; sumarle 1
+	str r2, [r1]			@; y guardarlo de nuevo
+	bl _gp_desinhibirIRQs
+	mov r0, #1				@; mover un 1 a R0 para indicar que el proceso se ha bloqueado correctamente
 
 .LreturnWait:
 
@@ -347,8 +362,13 @@ _gp_waitS:
 
 
 	.global _gp_signalS
+	@; Rutina para desbloquear un proceso.
+	@;Parametros
+	@; R0: semaforo del proceso a desbloqueaar (0..7)
+	@;Resultado
+	@; R0: 0 si no se ha desbloqueado, 1 si se ha desbloqueado
 _gp_signalS:
-	push {r1-r2, lr}
+	push {r1-r4, lr}
 
 	@; Comprobar que el semaforo existe
 	cmp r0, #7				@; comprobamos que el semaforo indicado no pasa de 7 (rango es 0-7)
@@ -365,13 +385,31 @@ _gp_signalS:
 	bhi .LreturnSignal		@; y salir de la rutina indicando que no se ha desbloqueado ningun proceso
 
 	@; Desbloquear proceso (liberar semaforo)
+	bl _gp_inhibirIRQs
 	mov r2, #1				@; en caso que si, mover un 1 en R2
 	strb r2, [r1, r0]		@; y guardarlo en el semaforo indicado por parametro
+
+	ldr r1, =_gd_qBlock		@; cargar direccion de la lista BLK
+	ldrb r2, [r1, r0]		@; extraer zocalo de la lista BLK en la posicion indicada por el semaforo (parametro)
+	mov r3, #0				@; mover un 0 en R3
+	strb r3, [r1, r0]		@; y guardarlo en la posicion de la lista BLK para restablecer su valor
+	ldr r1, =_gd_qReady		@; cargar direccion de la cola RDY
+	ldr r3, =_gd_nReady		@; cargar direccion del numero de procesos en RDY
+	ldr r4, [r3]			@; obtener su valor
+	strb r2, [r1, r4]		@; guardar zocalo en la cola RDY
+	add r4, #1				@; sumar 1 al numero de procesos RDY
+	str r4, [r3]			@; y guardarlo de nuevo
+
+	ldr r3, =_gd_nBlock		@; cargar direccion del numero de procesos en la lista BLK
+	ldr r4, [r3]			@; obtener su valor
+	sub r4, #1				@; restarle 1
+	str r4, [r3]			@; y guardarlo de nuevo
+	bl _gp_desinhibirIRQs
 	mov r0, #1				@; mover un 1 a R0 para indicar que se ha desbloqueado un proceso
 
 .LreturnSignal:
 
-	pop {r1-r2, pc}
+	pop {r1-r4, pc}
 
 
 	@; Rutina para actualizar la cola de procesos retardados, poniendo en
@@ -883,7 +921,6 @@ _gp_rsiTIMER0:
 	cmp r3, r2				@; comparamos si hemos visto todos los semaforos
 	blo .LcountBLK			@; si aun quedan, volver a iterar
 
-
 .LskipBLK:
 
 	mov r8, r4				@; ahora R8 contiene el numero total de workTicks
@@ -1025,33 +1062,7 @@ _gp_rsiTIMER0:
 
 .LnoreadBLK:
 
-@;	ldr r6, =_gp_msg
-@;	add r6, #3
-	@; escribir porcentaje de los procesos en la pantalla inferior
-@;.LnextItem:
-@;	mov r8, r0
-@;	ldr r7, [r8, #20]
-@;	cmp r7, #0
-@;	addeq r8, #24
-@;	beq .LnextItem
-
-@;.LintToASCII:
-@;	sub sp, #8				@; guardar espacio en la pila para el cociente y el resto
-@;	mov r0, r7				@; pasar el numerador
-@;	mov r1, #10				@; pasar el denominador
-@;	mov r2, sp				@; pasar direccion de memoria para el cociente
-@;	add r3, sp, #4			@; pasar direccion de memoria para el resto
-@;	bl _ga_divmod			@; hacer la división
-@;	pop {r4-r5}				@; R4 = cociente y R5 = resto
-
-@;	add r5, #'0'			@; sumar valor ASCII del caracter '0' para obtener valor ASCII del resultado
-@;	strb r5, [r6]
-@;	sub r6, #1
-@;	cmp r4, #0
-@;	addeq r4, #'0'
-@;	streqb r4, [r6]
-
-	@; TODO next
+	@; TODO (mostrar % en la pantalla inferior)
 
 	@; poner a 1 el bit 0 de _gd_sincMain para indicar al programa
 	@; principal que dispone de los porcentajes de uso de CPU
@@ -1061,10 +1072,5 @@ _gp_rsiTIMER0:
 	str r1, [r0]			@; y guardarlo de nuevo
 
 	pop {r0-r8, pc}
-
-@;_gp_msg:
-@;	.space 4
-
 	
 .end
-
